81983d2d1cb5dcf15da62752a5a97b5cf05422bc
Lina-go <77931393+Lina-go@users.noreply.github.com>
2021-04-16 12:11:29 -0500

    "Laboratorio 9-Entrega final"

--- App/view.py
+++ App/view.py
@@ -67,6 +67,7 @@ while True:
         print("\nInicializando....")
         # cont es el controlador que se usará de acá en adelante
         cont = controller.init()
+        
 
     elif int(inputs[0]) == 2:
         print("\nCargando información de crimenes ....")
